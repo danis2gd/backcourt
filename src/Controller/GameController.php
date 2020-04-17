639915d4693123ad20a46873c430e9cbeb4ae570
Daniel Chadwick <djchadwick@outlook.com>
2020-04-18 00:12:10 +0100

    BC schedule, start of game UI

---
+++ src/Controller/GameController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Game;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class PlayersController
+ * @package App\Controller
+ *
+ * @Route(name="app_games_", path="/games")
+ */
+class GameController extends AbstractController
+{
+    /**
+     * @Route(name="game", path="/{id}")
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function gameAction(int $id): Response
+    {
+        $game = $this->getDoctrine()
+            ->getRepository(Game::class)
+            ->findOneBy(['id' => $id]);
+
+        return $this->render(
+            'main/game/game.html.twig',
+            [
+                'game' => $game,
+            ]
+        );
+    }
+}
