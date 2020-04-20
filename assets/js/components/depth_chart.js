bdaf9f0dd059bb1b888a64dfa0cd28896aedfc98
Daniel Chadwick <djchadwick@outlook.com>
2020-04-21 00:48:01 +0100

    BC depth chart work

--- assets/js/components/depth_chart.js
+++ assets/js/components/depth_chart.js
@@ -1,3 +1,17 @@
 $(() => {
-    $('#depthchart').sortable();
+    let depthChart = $('#depthchart');
+    let depthChartData = [];
+
+    depthChart.sortable();
+
+    $('#save-depth-chart').on('click', (event) => {
+        event.preventDefault();
+
+        $('tr', depthChart).each((index, row) => {
+            row = $(row);
+            depthChartData.push(row.attr('data-player-id'));
+        });
+
+        console.log(depthChartData);
+    });
 });
